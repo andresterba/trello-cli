b1222bebd09d4503c466cd3f5caa5f846308b57d
André Sterba <git@sterba.dev>
2021-11-04 09:25:33 +0100

    Add context command

--- commands/context.go
+++ commands/context.go
@@ -25,5 +25,29 @@ func (command contextCommand) IsForCommand(commandParams []string) bool {
 }
 
 func (command contextCommand) Execute(commandParams []string) error {
+	config, err := getConfig()
+	if err != nil {
+		return err
+	}
+
+	newContext := commandParams[1]
+
+	if checkIfContextIsValid(newContext) {
+		config.DefaultContext = newContext
+	}
+
+	err = config.WriteConfig()
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func checkIfContextIsValid(context string) bool {
+	if context == PersonalContext || context == WorkContext {
+		return true
+	}
+
+	return false
 }
